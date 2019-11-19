e84a1471dfc61770b796dd42cc66c40cc6c0d9b2
Michael <3yebmb@gmail.com>
2019-11-19 19:05:20 +0500

    authorization and send message

--- app/src/main/java/dev/m13d/chatapp/MainActivity.java
+++ app/src/main/java/dev/m13d/chatapp/MainActivity.java
@@ -1,14 +1,91 @@
 package dev.m13d.chatapp;
 
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SIGN_IN_CODE = 1;
+    private RelativeLayout activity_main;
+    private FirebaseListAdapter<Message> adapter;
+    private FloatingActionButton btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity_main = findViewById(R.id.activity_main);
+        btnSend = findViewById(R.id.btn_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textField = findViewById(R.id.message_field);
+                if (textField.getText().toString().equals("")) return;
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail(),
+                                textField.getText().toString())
+                );
+                textField.setText("");
+            }
+        });
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Collections.singletonList(
+//                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+//                                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build())).build(),
+//                                    new AuthUI.IdpConfig.PhoneBuilder().build(),
+//                                    new AuthUI.IdpConfig.AnonymousBuilder().build())).build(),
+                                    SIGN_IN_CODE);
+        } else {
+            Snackbar.make(activity_main, "You're authorized", Snackbar.LENGTH_LONG).show();
+            displayAllMessages();
+        }
+    }
+
+    private void displayAllMessages() {
+        ListView listOfMessages = findViewById(R.id.list_of_messages);
+
+//        adapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+//            @Override
+//            protected void populateView(View v, Message model, int position) {
+//                TextView text_user, text_time, text_text;
+//                text_user = v.findViewById(R.id.message_user);
+//                text_time = v.findViewById(R.id.message_time);
+//                text_text = v.findViewById(R.id.message_text);
+//
+//                text_user.setText(model.getUserName());
+//                text_text.setText(model.getTextMessage());
+//                text_time.setText(DateFormat.format("dd-mm-yyyy HH:MM:SS", model.getMessageTime()));
+//            }
+//        };
+
+        listOfMessages.setAdapter(adapter);
     }
 }
