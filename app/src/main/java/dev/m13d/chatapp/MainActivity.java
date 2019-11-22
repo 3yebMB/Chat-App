0516582503fa871a5fa4b74a2928f038e35ec5ef
Michael <3yebmb@gmail.com>
2019-11-22 13:27:35 +0500

    add emoji

--- app/src/main/java/dev/m13d/chatapp/MainActivity.java
+++ app/src/main/java/dev/m13d/chatapp/MainActivity.java
@@ -3,7 +3,7 @@ package dev.m13d.chatapp;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,43 +14,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.github.library.bubbleview.BubbleTextView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Objects;
+
+import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SIGN_IN_CODE = 1;
     private RelativeLayout activity_main;
     private FirebaseListAdapter<Message> adapter;
-    private FloatingActionButton btnSend;
+    private EmojiconEditText emojiconEditText;
+    private ImageView emojiButton, sendButton;
+    private EmojIconActions emojIconActions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        activity_main = findViewById(R.id.activity_main);
-        btnSend = findViewById(R.id.btn_send);
+        initViews();
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText textField = findViewById(R.id.message_field);
-                if (textField.getText().toString().equals("")) return;
-                FirebaseDatabase.getInstance().getReference().push().setValue(
-                        new Message(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail(),
-                                textField.getText().toString())
-                );
-                textField.setText("");
-            }
-        });
+//        sendButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String mail = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail();
+//                FirebaseDatabase.getInstance().getReference().push(
+//                        new Message(mail, emojiconEditText.getText().toString())
+//                );
+//                emojiconEditText.setText("");
+//            }
+//        });
 
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             startActivityForResult(
@@ -69,15 +71,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void initViews() {
+        activity_main = findViewById(R.id.activity_main);
+        sendButton = findViewById(R.id.send_button);
+        emojiButton = findViewById(R.id.emoji_button);
+        emojiconEditText = findViewById(R.id.textField);
+
+        emojIconActions = new EmojIconActions(getApplicationContext(), activity_main, emojiconEditText, emojiButton);
+        emojIconActions.ShowEmojIcon();
+    }
+
     private void displayAllMessages() {
         ListView listOfMessages = findViewById(R.id.list_of_messages);
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("chats");
+//The error said the constructor expected FirebaseListOptions - here you create them:
+        FirebaseListOptions<Message> options = new FirebaseListOptions.Builder<Message>()
+                .setQuery(query, Message.class)
+                .setLayout(R.layout.list_item)
+                .build();
+        //Finally you pass them to the constructor here:
+        adapter = new FirebaseListAdapter<Message>(options){
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getTextMessage());
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+            }
+        };
+
+
+//        adapter = new FirebaseListAdapter<Message>() {
+//            @Override
+//            protected void populateView(@NonNull View v, @NonNull Message model, int position) {
+//
+//            }
+//        };
 
 //        adapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
 //            @Override
 //            protected void populateView(View v, Message model, int position) {
 //                TextView text_user, text_time;
 //                BubbleTextView text_text;
-
+//
 //                text_user = v.findViewById(R.id.message_user);
 //                text_time = v.findViewById(R.id.message_time);
 //                text_text = v.findViewById(R.id.message_text);
--- app/src/main/java/dev/m13d/chatapp/Message.java
+++ app/src/main/java/dev/m13d/chatapp/Message.java
@@ -3,13 +3,13 @@ package dev.m13d.chatapp;
 import java.util.Date;
 
 public class Message {
-    public String userName;
-    public String textMessage;
+    private String userName;
+    private String textMessage;
     private long messageTime;
 
     public Message() { }
 
-    public Message(String userName, String textMessage) {
+    Message(String userName, String textMessage) {
         this.userName = userName;
         this.textMessage = textMessage;
         this.messageTime = new Date().getTime();
